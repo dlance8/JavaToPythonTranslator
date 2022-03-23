056e6116f673e9ede5abbaff6cc06841116a7b4c
dlance8 <49040454+dlance8@users.noreply.github.com>
2022-03-23 10:50:27 -0400

    .

--- src/examples/testing.java
+++ src/examples/testing.java
@@ -1,28 +1,13 @@
 package examples;
+//import java.util.Scanner;
 
 public class testing {
-    private static void classMethod(){ // set indent 2
-        System.out.println("World");
-    }
     public static void main(String[] args){
-        int z = 3;
-        System.out.println("hello");
-        int y = 5;
-        classMethod();
-
-        int x = 4;
-
-        if(x < 5){
-            String a = "a";
-            System.out.println("x < 5");
-        }
-        else {
-            String b = "b";
-            System.out.println("x > 5");
-        }
-
-        for(int i = 0; i < 2; i++){
-            System.out.println(i);
-        }
+        int i = 0;
+        ++i;
+        i++;
+        i--;
+        --i;
     }
 }
+
